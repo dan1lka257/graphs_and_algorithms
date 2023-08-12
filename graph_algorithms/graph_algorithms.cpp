c17b7a8122959607cb8fe8aa47f8b7c4582610b0
dan1lka257 <daniltursunov597@gmail.com>
2023-08-12 16:12:13 +0300

    delete OOP & add edge graph input

--- graph_algorithms/graph_algorithms.cpp
+++ graph_algorithms/graph_algorithms.cpp
@@ -5,41 +5,25 @@
 
 using namespace std;
 
-class Node {
-    friend Graph;
-    Node(); // TODO node constructor
-    void push(int id, int color) {
-        neighbours.push_back(make_pair(id, color));
-    }
-    int marked = 0;
-    int id; // TODO correct id maker 
-    // TODO copying operator
-    vector<pair<int, int>> neighbours;
-};
-
-class Graph {
-    vector<Node> nodes;
-    int verticles_number = 0;
-    Graph() {
-        int nodes_number;
-        cin >> nodes_number;
-        vector<vector<int>> temp_nodes(nodes_number, vector<int>());
-        for (int i = 0; i < nodes_number; i++) {
-            int node_number;
-            Node versus(); // TODO clearify this moment
-            while (cin >> node_number) {
-                temp_nodes[i].push_back(node_number);
-                versus.push(node_number);
-            }
-        }
-    }
-};
-
-// TODO write basic BFS algorithm
-
-// TODO write funcs for graph
-
 int main()
 {
     std::cout << "Hello World!\n";
+    int n, m;
+    cin >> n >> m;
+    vector<vector<pair<int, int>>> graph(n, vector<pair<int, int>> ());
+    for (int i = 0; i < m; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        graph[a].push_back(make_pair(b, c));
+        graph[b].push_back(make_pair(a, c));
+    }
+    /* DEBUG GRAPH COUT
+    for (int i = 0; i < n; i++) {
+        cout << i << ": ";
+        for (int j = 0; j < graph[i].size(); j++) {
+            cout << "in" << graph[i][j].first << "for" << graph[i][j].second << " ";
+        }
+        cout << endl;
+    }
+    */
 }
